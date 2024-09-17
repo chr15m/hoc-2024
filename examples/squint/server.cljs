2ae12b80030a743057204c5735d231d2a40b6e29
Chris McCormick <chris@mccormick.cx>
2024-09-17 21:58:12 +0100

    Simplify squint html component.

--- examples/squint/server.cljs
+++ examples/squint/server.cljs
@@ -2,22 +2,23 @@
   (:require ["express$default" :as express]))
 
 (defn component-homepage [req]
-  #html [:html
-         [:body "Hello "
-          (or (:name req.query)
-              "unknown")]
-         [:ul
-          [:li 1]
-          [:li 2]
-          [:li 3]
-          (map (fn [i]
-                 #html [:li (inc i)]) [3 4 5])]])
+  #html
+  [:html
+   [:body "Hello "
+    (or (:name req.query)
+        "unknown")]
+   [:ul
+    [:li 1]
+    [:li 2]
+    [:li 3]
+    (map (fn [i]
+           #html [:li (inc i)]) [3 4 5])]])
 
 (defn serve-homepage [req res]
-  (.send res (aget (component-homepage req) "s")))
+  (.send res (str (component-homepage req))))
 
 (let [app (express)]
-  (.get app "/testpage" #(serve-homepage %1 %2))
-  (.use app "/" (.static express "public"))
+  (.get app "/$" #(serve-homepage %1 %2))
+  (.use app "/" (.static express "frontend/public"))
   (.listen app 8000)
   (println "Server running on port 8000."))
